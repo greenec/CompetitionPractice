0f606b57aae6fc3ba4c9bf231a90e06266ca3d5d
greenec <connor.greene2000@gmail.com>
2015-11-02 10:58:58 -0500

    11/2/15

---
+++ Practice/Practice/Enemies.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    class Enemies
+    {
+        public static void Main(string[] args)
+        {
+
+            int count = 0;
+
+            Console.Write("How many seats: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+            int n = (int)Math.Pow(input, 2);
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                String binary = Convert.ToString(i, 2);
+
+                if (!binary.Contains("11"))
+                    count++;
+            }
+            Console.WriteLine(count);
+            Console.ReadLine();
+        }
+    }
+}
